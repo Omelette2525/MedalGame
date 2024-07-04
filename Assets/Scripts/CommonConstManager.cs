3385f88d37d7a1fd3eaeb742a417bda86fe9251e
T.Y <yuki.t20040821@gmail.com>
2024-07-04 17:07:25 +0900

    途中経過 共通定数を定義する名前空間を作成 イベントの実行順を管理するcsを作成中

---
+++ Assets/Scripts/CommonConstManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonConst
+{
+    /* 定数管理用のクラス */
+    public static class CommonConstManager
+    {
+        /* イベントの順番管理用 入力時と、判定時に使う*/
+        public const int SC = 1;
+        public const int BRIGHTJPC = 2;
+        public const int SHADOWJPC = 3;
+    }
+}
--- Assets/Scripts/SCManager.cs
+++ Assets/Scripts/SCManager.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Cinemachine;
 using UnityEngine;
+using CommonConst;
 
 public class SCManager : MonoBehaviour
 {
@@ -117,7 +118,6 @@ public class SCManager : MonoBehaviour
         SCRotateScript.rotateFlag = true; // 回転開始
         isSC = false; // SC中フラグを無効にする
         Debug.Log("フラグ");
-        await WaitTaskAsync(WAIT); // この関数はUpdate内でawaitされていないので、このTaskの終了を待つことなくアップデートが実行されてしまう
     }
     public void SCStockPlus()
     {
