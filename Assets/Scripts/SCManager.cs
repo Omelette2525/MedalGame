cf3c571c01be461a9af4212d8895ad8e537e9d82
T.Y <yuki.t20040821@gmail.com>
2024-08-07 03:25:17 +0900

    Update SCManager.cs

--- Assets/Scripts/SCManager.cs
+++ Assets/Scripts/SCManager.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Cinemachine;
 using UnityEngine;
-using CommonConst;
+using CommonConst; // 共通定数を扱うためにusing
 
 public class SCManager : MonoBehaviour
 {
